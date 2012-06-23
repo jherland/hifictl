2c4ddb72defe3da1484d7e07e0f81be1f2ddcc3a
Johan Herland <johan@herland.net>
2012-06-23 04:32:51 +0200

    Rewrite part #1: Add base class for A/V devices

---
+++ av_device.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python2
+
+
+class AV_Device(object):
+	"""Encapsulate an A/V device that can be controlled from av_control.
+
+	The device must be hooked up to some file descriptor that can be passed
+	to epoll. It must also declare a namespace which is used to select which
+	commands to pass to its handle_cmd() method.
+
+	Finally, this class declares methods for handling incoming epoll events
+	and A/V commands."""
+
+	Description = "Unspecified A/V device"
+
+	Debug = False
+
+	@staticmethod
+	def human_readable(s):
+		"""Convenience method for making byte strings human-readable.
+
+		Returns the given string with all non-human-readable chars
+		replaced by their respective hax code (formatted as \0x##).
+		"""
+		ret = ""
+		for c in s:
+			i = ord(c)
+			if i >= 0x20 and i < 0x7f:
+				ret += c
+			else:
+				ret += "\\0x%02x" % (ord(c))
+		return ret
+
+	def debug(self, ts, s):
+		"""Convenience method for debug output."""
+		if self.Debug:
+			print "%.2f: %s" % (ts, self), s
+
+	def __init__(self, cmd_namespace):
+		self.cmd_namespace = cmd_namespace
+		self.cmd_dispatcher = None
+
+	def __str__(self):
+		return "<%s %s>" % (self.__class__.__name__, self.cmd_namespace)
+
+	def register(self, epoll, cmd_dispatcher):
+		"""Register this device's file desc. and event mask with epoll.
+
+		Also, return this device's file descriptor, so that the
+		surrounding epoll loop can map events back to this object.
+
+		Finally, use the given command dispatcher to dispatch A/V
+		commands to the surrounding system.
+		"""
+		raise NotImplementedError
+
+	def handle_events(self, epoll, events, ts = 0):
+		"""Handle the given events code from the given epoll object.
+
+		The given code is a bitwise combination of the events returned
+		from self.eventmask().
+
+		This method is free to call epoll.modify()
+		"""
+		raise NotImplementedError
+
+	def handle_cmd(self, cmd, ts = 0):
+		"""Handle the given A/V control command.
+
+		Only A/V control commands of the form "$namespace $cmd..." where
+		$namespace equals self.cmd_namespace are passed to this method,
+		and only the "$cmd..." part of the command is passed.
+		"""
+		raise NotImplementedError
