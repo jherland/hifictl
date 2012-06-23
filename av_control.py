571cd1390e7d78bea2edab64a29fa5bcfae84d63
Johan Herland <johan@herland.net>
2012-06-23 21:18:34 +0200

    Rewrite part #5: Rewrite av_control.py from scratch, using the AV_Device infrastructure
    Also use argparse to get a nicer command-line interface

--- av_control.py
+++ av_control.py
@@ -14,112 +14,96 @@ Keep as little local state as possible
 import sys
 import os
 import time
-import atexit
+import select
+import argparse
 
 from hdmi_switch import HDMI_Switch
-from avr_conn import AVR_Connection
-from avr_status import AVR_Status
-from avr_command import AVR_Command
+from avr_device import AVR_Device
+from av_fifo import AV_FIFO
 
 
-# Connection point to clients that want to control the AVR
-fifo_name = "/tmp/av_control"
-
-# Where the HDMI switch is connected
-HDMI_tty = "/dev/ttyUSB0"
-
-# Where the AVR itself is connected
-AVR_tty = "/dev/ttyUSB1"
-
-# Map FIFO command to corresponding HDMI command
-HDMI_Map = {
-	"hdmi off": "off",
-	"hdmi 1": "1",
-	"hdmi 2": "2",
-	"hdmi 3": "3",
-	"hdmi 4": "4",
-}
-
-# Map FIFO command to corresponding AVR command
-AVR_Map = {
-	"avr on":   "POWER ON",
-	"avr off":  "POWER OFF",
-	"avr mute": "MUTE",
-	"avr vol+": "VOL UP",
-	"avr vol-": "VOL DOWN",
-}
+Devices = (
+	{
+		'name':         "hdmi",
+		'class':        HDMI_Switch,
+		'default_path': "/dev/ttyUSB0",
+		'path_help':    "Path to tty connected to HDMI switch",
+	},
+	{
+		'name':         "avr",
+		'class':        AVR_Device,
+		'default_path': "/dev/ttyUSB1",
+		'path_help':    "Path to tty connected to AVR",
+	},
+	{
+		'name':         "fifo",
+		'class':        AV_FIFO,
+		'default_path': "/tmp/av_control",
+		'path_help':    "Path to A/V command FIFO",
+	},
+)
 
 
-def destroy_fifo():
+def main(args):
+	parser = argparse.ArgumentParser(
+		description = "Controller daemon for A/V devices")
+
+	for dev in Devices:
+		parser.add_argument("--%s" % (dev['name']),
+			default = dev['default_path'],
+			help = "%s (default: %%(default)s)" % (dev['path_help']),
+			metavar = "PATH")
+
+	parsed_args = vars(parser.parse_args(args))
+
+	epoll = select.epoll()
+	t_start = time.time()
+
+	devices = {} # Map device namespaces to AV_Device objects.
+	device_fds = {} # Map device fds to AV_Device objects.
+
+	def cmd_dispatcher(cmd):
+		"""Dispatch commands to their appropriate device handlers.
+
+		Parse command strings of the form "$namespace $subcmd...",
+		and pass "$subcmd..." on to the device named $namespace.
+		"""
+		ts = time.time() - t_start
+		print "cmd_dispatcher(%s)" % (cmd)
+		namespace, subcmd = cmd.split(" ", 1)
+		if namespace in devices:
+			devices[namespace].handle_cmd(subcmd, ts)
+		else:
+			print "Unknown namespace '%s'" % (namespace)
+
+	for dev in Devices:
+		try:
+			namespace = dev['name']
+			cls = dev['class']
+			print "*** Initializing %s..." % (cls.Description),
+			dev = cls(namespace, parsed_args[namespace])
+			dev_fd = dev.register(epoll, cmd_dispatcher)
+			devices[namespace] = dev
+			device_fds[dev_fd] = dev
+			print "done"
+		except Exception as e:
+			print e
+
+	if not devices:
+		print "No devices found. Aborting..."
+		return 1
+
+	print "Starting A/V controller main loop"
 	try:
-		os.remove(fifo_name)
-	except:
-		pass
+		while True:
+			for fd, events in epoll.poll():
+				ts = time.time() - t_start
+				assert fd in device_fds
+				device_fds[fd].handle_events(epoll, events, ts)
+	except KeyboardInterrupt:
+		print "Aborted by user"
 
-
-def create_fifo():
-	# Open FIFO for reading commands from clients
-	if os.path.exists(fifo_name):
-		raise OSError("%s exists. Another instance of %s running?" % (
-			fifo_name, os.path.basename(sys.argv[0])))
-	os.mkfifo(fifo_name)
-	atexit.register(destroy_fifo)
-	return os.open(fifo_name, os.O_RDONLY | os.O_NONBLOCK)
-
-
-def usage(msg):
-	print msg + ":"
-	print "Usage:"
-	print "  %s [--hdmi <serial_port>] [--avr <serial_port>]" % (sys.argv[0])
-	print
-	print "  Then write any of the following commands to %s:" % (fifo_name)
-	for cmd in sorted(AVR_Map.keys()):
-		print "    %s" % (cmd)
-	return 1
-
-
-def main(args, HDMI_tty = HDMI_tty, AVR_tty = AVR_tty):
-	if len(args) >= 2 and args[0] == "--hdmi":
-		HDMI_tty = args[1]
-		args = args[2:]
-	if len(args) >= 2 and args[0] == "--avr":
-		AVR_tty = args[1]
-		args = args[2:]
-
-	if args:
-		return usage("Unknown arg(s): '%s'" % (" ".join(args)))
-
-	fifo_fd = create_fifo()
-	hdmi = HDMI_Switch(HDMI_tty)
-	avr = AVR_Connection(AVR_tty)
-
-	prev_dgram = None
-	fifo_input = ""
-	ts = time.time()
-	while True:
-		fifo_input += os.read(fifo_fd, 64)
-		cmds = fifo_input.split("\n")
-		fifo_input = cmds.pop()
-		for cmd in cmds:
-			if cmd in HDMI_Map:
-				hdmi.send_command(HDMI_Map[cmd])
-			elif cmd in AVR_Map:
-				avr.send_dgram(AVR_Command(AVR_Map[cmd]).dgram())
-			else:
-				print "Unknown command '%s'" % (cmd)
-		dgram = avr.write_and_recv()
-		if dgram == prev_dgram:
-			continue # Skip if unchanged
-		prev_dgram = dgram
-
-		status = AVR_Status.from_dgram(dgram)
-
-		now = time.time()
-		print "%s (period: %f seconds)" % (status, now - ts)
-		ts = now
-
-	avr.close()
-	os.close(fifo_fd)
+	epoll.close()
 	return 0
 
 
