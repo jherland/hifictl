f7fad038121efb725f873c0d5d10f856635292b1
Johan Herland <johan@herland.net>
2012-06-24 03:20:37 +0200

    Update XBMC script and keymap to directly forward commands to /tmp/av_control
    This means we no longer have to edit the addon script to add new A/V commands.
    Just specify them explicitly in the keyboard re-mapping, instead.

--- xbmc/script.av-control-client/default.py
+++ xbmc/script.av-control-client/default.py
@@ -8,28 +8,13 @@ import sys
 
 av_fifo = "/tmp/av_control"
 
-cmd_map = {
-	"VolumeUp":    "avr vol+",
-	"VolumeDown":  "avr vol-",
-	"Mute":        "avr mute",
-	"HDMI1":       "hdmi 1",
-	"HDMI2":       "hdmi 2",
-	"HDMI3":       "hdmi 3",
-	"HDMI4":       "hdmi 4",
-#	"SurroundOn":  "surround ???",
-}
+assert len(sys.argv) == 2
+av_cmd = sys.argv[1].strip()
 
-assert len(sys.argv) > 0
-args = sys.argv[1:]
-
-if args and args[0] in cmd_map:
-	if os.path.exists(av_fifo):
-#		print "Sending '%s' to %s" % (cmd_map[args[0]], av_fifo)
-		f = open(av_fifo, "w")
-		print >>f, cmd_map[args[0]]
-		f.close()
-	else:
-		print "Cannot forward command '%s': %s does not exist" % (
-			args[0], av_fifo)
+if os.path.exists(av_fifo):
+#	print "Sending '%s' to %s" % (av_cmd, av_fifo)
+	f = open(av_fifo, "w")
+	print >>f, av_cmd
+	f.close()
 else:
-	print "Unknown command '%s'" % (", ".join(args))
+	print "Cannot forward '%s': %s does not exist" % (av_cmd, av_fifo)
