9cdee51db6fb7e1fb744c0dac8af812379a56825
Johan Herland <johan@herland.net>
2012-06-27 01:19:34 +0200

    Rewrite AV_SerialDevice to use AV_Loop. Include new ready_to_write logic, that puts the decision of whether to poll for writability _inside_ ready_to_write()

--- av_serial_device.py
+++ av_serial_device.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python2
 
-import sys
 import serial
-import select
 
 from av_device import AV_Device
 
@@ -32,8 +30,8 @@ class AV_SerialDevice(AV_Device):
 				ret += "\\0x%02x" % (ord(c))
 		return ret
 
-	def __init__(self, cmd_namespace, tty, baudrate):
-		AV_Device.__init__(self, cmd_namespace)
+	def __init__(self, av_loop, name, tty, baudrate):
+		AV_Device.__init__(self, av_loop, name)
 
 		# It seems pyserial needs the rtscts flag toggled in
 		# order to communicate consistently with the remote end.
@@ -41,67 +39,63 @@ class AV_SerialDevice(AV_Device):
 		self.ser.rtscts = False
 		self.ser.timeout = 0 # Non-blocking reads
 
-		self.epoll = None
-		self.epoll_events = select.EPOLLIN | select.EPOLLPRI \
-			| select.EPOLLERR | select.EPOLLHUP
-
 		self.write_queue = []
+		self.write_ready = True
 
-	def register(self, epoll, cmd_dispatcher = None):
-		self.epoll = epoll
-		self.epoll.register(self.ser.fileno(), self.epoll_events)
-		return self.ser.fileno()
+		self.av_loop.add_handler(self.ser.fileno(), self.handle_io,
+			self.av_loop.READ)
 
-	def handle_events(self, epoll, events, ts = 0):
-		ret = None
-		assert epoll == self.epoll
-		if events & select.EPOLLIN:
-			try:
-				ret = self.handle_read(ts)
-			except Exception as e:
-				self.debug(ts, "handle_read(): %s" % (e))
-		if events & select.EPOLLOUT:
-			try:
-				if not self.handle_write(ts):
-					# Nothing more to write, reset eventmask
-					self.epoll.modify(self.ser.fileno(),
-						self.epoll_events)
-			except Exception as e:
-				self.debug(ts, "handle_write(): %s" % (e))
+	def handle_io(self, fd, events):
+		assert fd == self.ser.fileno()
+#		self.debug("handle_io(%i, %i)" % (fd, events))
+		if events & self.av_loop.READ:
+#			try:
+				self.handle_read()
+#			except Exception as e:
+#				self.debug("handle_read(): %s" % (e))
+		if events & self.av_loop.WRITE:
+#			try:
+				self.handle_write()
+#			except Exception as e:
+#				self.debug("handle_write(): %s" % (e))
 
-		events &= ~(select.EPOLLIN | select.EPOLLOUT)
+		events &= ~(self.av_loop.READ | self.av_loop.WRITE)
 		if events:
-			self.debug(ts, "Unhandled events: %u" % (events))
-		return ret
+			self.debug("Unhandled events: %u" % (events))
 
-	def ready_to_write(self, ts = 0, set_to = None):
+	def ready_to_write(self, set_to = None):
 		"""Return whether or not the remote end is ready to receive.
 
-		This method should probably be overridden in subclasses."""
-		return ts > 0.5
+		This method should probably be extended in subclasses."""
+		if set_to is not None:
+			self.write_ready = set_to
 
-	def handle_read(self, ts):
-		"""Attempt to read a datagram from the serial port.
+		ret = self.write_ready and self.write_queue
+		eventmask = self.av_loop.READ
+		if ret:
+			eventmask |= self.av_loop.WRITE
+		self.av_loop.update_handler(self.ser.fileno(), eventmask)
+		return ret
+
+	def handle_read(self):
+		"""Attempt to read data from the serial port.
 
 		This method should probably be overridden in subclasses.
 		"""
-		return self.human_readable(self.ser.read(1024))
+		print self.human_readable(self.ser.read(64 * 1024))
 
-	def handle_write(self, ts):
-		"""Attempt to write a datagram to the serial port."""
-		if self.write_queue and self.ready_to_write(ts):
+	def handle_write(self):
+		"""Attempt to write data to the serial port."""
+		if self.ready_to_write():
 			data = self.write_queue.pop(0)
 			written = self.ser.write(data)
 			assert written == len(data)
-			self.ready_to_write(ts, False)
-			self.debug(ts, "Wrote %u bytes (%s)" % (written,
+			self.ready_to_write(False)
+			self.debug("Wrote %u bytes (%s)" % (written,
 				" ".join(["%02x" % (ord(b)) for b in data])))
-		return len(self.write_queue)
 
-	def schedule_write(self, ts, data):
-		self.debug(ts, "Adding %u bytes to write queue (%s)" % (
-			len(data), " ".join(["%02x" % (ord(b)) for b in data])))
-		if not self.write_queue:
-			self.epoll.modify(self.ser.fileno(),
-				self.epoll_events | select.EPOLLOUT)
+	def schedule_write(self, data):
+		self.debug("Adding %u bytes to write queue (%s)" % (len(data),
+			" ".join(["%02x" % (ord(b)) for b in data])))
 		self.write_queue.append(data)
+		self.ready_to_write()
