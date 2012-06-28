7c2d10b73042acbf83a8b6108618ea2af1780fe0
Johan Herland <johan@herland.net>
2012-06-28 16:24:58 +0200

    Rewrite http_server.py in terms of AV_Loop and tornado. Much simpler and shorter

--- http_server.py
+++ http_server.py
@@ -1,119 +1,56 @@
 #!/usr/bin/env python2
 
 import os
-import select
-import time
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from tornado.web import RequestHandler, Application, StaticFileHandler
 
 from av_device import AV_Device
 
 
-class AV_ReqHandler(BaseHTTPRequestHandler):
+class AV_CommandHandler(RequestHandler):
 
-	server_version = "AV_httpd/0.1"
-	protocol_version = "HTTP/1.0"
-
-	StaticPaths = {
-		"/favicon.ico": "image/x-icon"
-	}
-
-	# The following seems to be needed with Opera clients, because a
-	# successful request from Opera will be immediately followed by a
-	# new null-request, which blocks the read on the server socket for
-	# many seconds.
-	timeout = 0.01
-
-	def send_content(self, data):
-		self.send_header("Connection", "close")
-		self.send_header("Content-Length", len(data))
-		self.end_headers()
-		self.wfile.write(data)
-
-	def do_static(self):
-		content_type = self.StaticPaths[self.path]
-
-		self.send_response(200)
-		self.send_header("Content-Type", content_type)
-
-		f = open(os.path.join(self.server.root, self.path.lstrip("/")))
-		self.send_content(f.read())
-		f.close()
-
-	def do_GET(self):
-		if self.path in self.StaticPaths:
-			return self.do_static()
-
+	def get(self, path):
 		# Turn self.path into an A/V command and submit it
-		cmd = self.path.strip("/").replace("/", " ")
-		self.server.cmd_dispatcher(cmd)
+		cmd = path.strip("/").replace("/", " ")
+		self.application.av_loop.submit_cmd(cmd)
 
-		# Send data back to the client
-		self.send_response(200)
-		self.send_header("Content-Type", "text/html")
-
-		lines = []
-		lines.append("<html><head></head><body>")
-		lines.append("<p>%s %s</p>" % (self.server, self.server.cookie))
-		lines.append("<p>%s -> %s</p>" % (self.path, cmd))
-		lines.append("<p>%s</p>" % (self.request_version))
-		lines.append("<pre>%s</pre>" % (self.headers))
-		lines.append("</body></html>")
-		self.send_content("\n".join(lines))
+		self.write("Received command '%s'" % (cmd))
 
 
-class AV_HTTPServer(HTTPServer, AV_Device):
+class AV_HTTPServer(AV_Device, Application):
 
 	Description = "A/V controller HTTP server"
 
-	def __init__(self, cmd_namespace = "http", root = "./http",
-		     address = ('', 8000), ReqHandlerClass = AV_ReqHandler):
-		HTTPServer.__init__(self, address, ReqHandlerClass)
-		AV_Device.__init__(self, cmd_namespace)
+	Debug = True ###
 
-		self.root = root
-		self.cookie = 0
+	def __init__(self, av_loop, name = "http", address = ("", 8000),
+			static_root = "./http"):
+		AV_Device.__init__(self, av_loop, name)
+		Application.__init__(self, [
+			(r"/static/(.*)",   StaticFileHandler,
+				{"path": static_root}),
+			(r"/(favicon.ico)", StaticFileHandler,
+				{"path": static_root}),
+			(r"/(.*)",          AV_CommandHandler),
+		], debug = self.Debug)
 
-	def register(self, epoll, cmd_dispatcher):
-		self.cmd_dispatcher = cmd_dispatcher
-		epoll.register(self.fileno(), select.EPOLLIN)
-		return self.fileno()
-
-	def handle_events(self, epoll, events, ts = 0):
-		if events & select.EPOLLIN:
-			self.cookie = ts
-#			t_start = time.time()
-			self.handle_request()
-#			elapsed = time.time() - t_start
-#			self.debug(ts, "Handled request in %.2fs" % (elapsed))
-		events &= ~select.EPOLLIN
-		if events:
-			self.debug(ts, "Unhandled events: %u" % (events))
+		self.address = address
+		self.listen(self.address[1], self.address[0])
 
 
 def main(args):
-	httpd = AV_HTTPServer()
-	epoll = select.epoll()
+	from av_loop import AV_Loop
 
-	def cmd_dispatcher(cmd):
-		print "cmd_dispatcher(%s)" % (cmd)
+	mainloop = AV_Loop()
 
-	httpd.register(epoll, cmd_dispatcher)
-	print "Browse to %s:%u (Ctrl-C here to stop me)" % (
-		httpd.server_name, httpd.server_port)
+	httpd = AV_HTTPServer(mainloop)
 
-	t_start = time.time()
-	ts = 0
-	try:
-		while True:
-			for fd, events in epoll.poll():
-				ts = time.time() - t_start
-				assert fd == httpd.fileno()
-				httpd.handle_events(epoll, events, ts)
-	except KeyboardInterrupt:
-		print "Aborted by user"
+	def cmd_dispatcher(namespace, subcmd):
+		print " -> cmd_dispatcher(%s, %s)" % (namespace, subcmd)
+	mainloop.add_cmd_handler("", cmd_dispatcher)
 
-	epoll.close()
-	return 0
+	print "Browse to http://%s:%u/ (Ctrl-C here to stop me)" % (
+		httpd.address[0] or "localhost", httpd.address[1])
+	return mainloop.run()
 
 
 if __name__ == '__main__':
