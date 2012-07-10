57812f2c451be0ea45b5afd7991acf20b5730044
Johan Herland <johan@herland.net>
2012-07-10 22:30:39 +0200

    Rewrite fake_avr.py to use AV_Loop/AV_Device and proper read polling

--- fake_avr.py
+++ fake_avr.py
@@ -5,14 +5,16 @@ import os
 import fcntl
 import termios
 import time
+from tornado.ioloop import PeriodicCallback
 
+from av_device import AV_Device
 from timed_queue import TimedQueue
 from avr_dgram import AVR_Datagram
 from avr_status import AVR_Status
 from avr_command import AVR_Command
 
 
-class Fake_SerialDevice(object):
+class Fake_SerialDevice(AV_Device):
 	"""Create a local serial-port-like device that can be used to
 	impersonate real devices connected to a serial port.
 
@@ -20,7 +22,11 @@ class Fake_SerialDevice(object):
 	device when the serial device is not available.
 	"""
 
-	def __init__(self):
+	Description = "Fake serial port device"
+
+	def __init__(self, av_loop, name):
+		AV_Device.__init__(self, av_loop, name)
+
 		self.master, self.slave = pty.openpty()
 		self._client_name = os.ttyname(self.slave)
 
@@ -37,6 +43,9 @@ class Fake_SerialDevice(object):
 		newterm[3] = newterm[3] & ~termios.ECHO # lflags
 		termios.tcsetattr(self.master, termios.TCSAFLUSH, newterm)
 
+		self.av_loop.add_handler(self.master, self.handle_io,
+			self.av_loop.READ)
+
 	def __del__(self):
 		# Close the remaining descriptor
 		termios.tcsetattr(self.master, termios.TCSAFLUSH, self.term)
@@ -48,13 +57,49 @@ class Fake_SerialDevice(object):
 	def client_name(self):
 		return self._client_name
 
+	def handle_io(self, fd, events):
+		assert fd == self.master
+#		self.debug("handle_io(%i, %i)" % (fd, events))
 
-class Fake_AVR(object):
+		if events & self.av_loop.READ:
+#			try:
+				self.handle_read()
+#			except Exception as e:
+#				self.debug("handle_read(): %s" % (e))
+		if events & self.av_loop.WRITE:
+#			try:
+				self.handle_write()
+#			except Exception as e:
+#				self.debug("handle_write(): %s" % (e))
+		if events & self.av_loop.ERROR:
+			# Ignore HUP and EIO, etc. FIXME: Is this safe?
+			pass
+
+		events &= ~(self.av_loop.READ | self.av_loop.WRITE | self.av_loop.ERROR)
+		if events:
+			self.debug("Unhandled events: %u" % (events))
+
+
+	def handle_read(self):
+		"""Attempt to read data from the PTY.
+
+		This method should probably be overridden in subclasses.
+		"""
+		print os.read(self.master, 64 * 1024)
+
+	def handle_write(self):
+		"""Must be overridden in subclasses that poll for writes."""
+		raise NotImplementedError
+
+
+class Fake_AVR(Fake_SerialDevice):
 	"""Impersonate an AVR unit.
 
 	Receive remote commands, update internal state and provide plausible
 	AVR_Status messages.
 	"""
+
+	Description = "Fake Harman/Kardon AVR 430"
 
 	EmptyIcons = chr(0x00) * 14
 	DefaultIcons = "".join(map(chr, [0xc0, 0x00, 0x00, 0x00,
@@ -67,8 +112,11 @@ class Fake_AVR(object):
 		"volume":  ("FAKE AVR      ", "  VOL %(volume)3i dB  ", DefaultIcons),
 	}
 
-	def __init__(self):
-		self.t0 = time.time()
+	RecvDGramSpec = ("PCSEND", 2, 4) # Receive PC->AVR remote commands
+	SendDGramSpec = ("MPSEND", 3, 48) # Send AVR->PC status updates
+
+	def __init__(self, av_loop, name):
+		Fake_SerialDevice.__init__(self, av_loop, name)
 
 		self.standby = True
 		self.mute    = False
@@ -76,9 +124,33 @@ class Fake_AVR(object):
 
 		self.status_queue = TimedQueue(self.gen_status("standby"))
 
+		self.write_timer = PeriodicCallback(self.write_now, 50, av_loop)
+		self.write_timer.start()
+
+		self.recv_dgram_len = AVR_Datagram.full_dgram_len(self.RecvDGramSpec)
+		self.recv_data = "" # Receive buffer
+
+		self.t0 = time.time()
+
+	def __del__(self):
+		self.write_timer.stop()
+
+	def write_now(self):
+		os.write(self.master, AVR_Datagram.build_dgram(
+			self.status().dgram(), self.SendDGramSpec))
+
 	def status(self):
 		"""Return AVR_Status diagram for current state."""
 		return self.status_queue.current()
+
+	def handle_read(self):
+		self.recv_data += os.read(self.master, 1024)
+		while len(self.recv_data) >= self.recv_dgram_len:
+			dgram = self.recv_data[:self.recv_dgram_len]
+			self.recv_data = self.recv_data[self.recv_dgram_len:]
+			self.handle_command(AVR_Command.from_dgram(
+				AVR_Datagram.parse_dgram(dgram,
+					self.RecvDGramSpec)))
 
 	def gen_status(self, key):
 		line1, line2, icons = self.StatusMap[key]
@@ -107,37 +179,21 @@ class Fake_AVR(object):
 
 
 def main(args):
-	tty = Fake_SerialDevice()
-	print "You can now start ./av_control.py --avr %s" % (tty.client_name())
+	import argparse
 
-	# Repeatedly send status info every 5/100 seconds, until user aborts
-	avr = Fake_AVR()
-	recv_data = "" # Receive buffer
-	recv_dgram_spec = ("PCSEND", 2, 4) # Receive PC->AVR remote commands
-	send_dgram_spec = ("MPSEND", 3, 48) # Send AVR->PC status updates
-	recv_dgram_len = AVR_Datagram.full_dgram_len(recv_dgram_spec)
-	try:
-		while True:
-			try:
-				recv_data += os.read(tty.fileno(), 1024)
-			except OSError as e:
-				if e.errno not in (5, 11): # EIO or EAGAIN
-					raise e
-			while len(recv_data) >= recv_dgram_len:
-				dgram = recv_data[:recv_dgram_len]
-				recv_data = recv_data[recv_dgram_len:]
-				avr.handle_command(AVR_Command.from_dgram(
-					AVR_Datagram.parse_dgram(
-						dgram, recv_dgram_spec)))
+	from av_loop import AV_Loop
 
-			time.sleep(0.05)
-			os.write(tty.fileno(), AVR_Datagram.build_dgram(
-				avr.status().dgram(), send_dgram_spec))
-	except KeyboardInterrupt:
-		pass
+	parser = argparse.ArgumentParser(
+		description = Fake_AVR.Description)
+	Fake_AVR.register_args("avr", parser)
 
-	del tty
-	return 0
+	mainloop = AV_Loop(vars(parser.parse_args(args)))
+	avr = Fake_AVR(mainloop, "avr")
+
+	print "You can now start ./av_control.py --avr-tty %s" % (
+		avr.client_name())
+
+	return mainloop.run()
 
 
 if __name__ == '__main__':
