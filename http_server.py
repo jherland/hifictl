d051a1a6aca4480053a1e85837788b91c9008fd1
Johan Herland <johan@herland.net>
2012-08-08 07:57:00 +0200

    Remove unused template arguments

--- http_server.py
+++ http_server.py
@@ -20,9 +20,7 @@ class IndexHandler(RequestHandler):
 			avr_state = None
 
 		self.write(app.templates.load("index.html").generate(
-			avr_state = avr_state,
 			cmd_handlers = app.av_loop.cmd_handlers,
-			last_cmd = self.get_argument("cmd", None),
 		))
 
 
