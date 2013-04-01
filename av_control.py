e8b93cba919e1aba97c810e943e23e55f539db45
Johan Herland <johan@herland.net>
2013-04-01 14:14:00 +0200

    Use python instead of python2

--- av_control.py
+++ av_control.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 """
 Manage I/O between a collection of A/V devices.
--- av_device.py
+++ av_device.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 
 class AV_Device(object):
