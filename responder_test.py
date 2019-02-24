b20065b7e55c6a5c1ff0845bfa006a76164048af
Johan Herland <johan@herland.net>
2019-02-24 03:20:26 +0100

    responder_test.py: First attempt at integrating surround_receiver_hk

--- responder_test.py
+++ responder_test.py
@@ -1,17 +1,65 @@
 #!/usr/bin/env python3
 
 import asyncio
+import logging
 import responder
 
+from surround_receiver_hk import HarmanKardon_Surround_Receiver
+
+
+logger = logging.getLogger(__name__)
 
 api = responder.API(static_dir='http_static')
 api.add_route("/", static=True)
 
+audio_commands = asyncio.Queue()
+audio_states = asyncio.Queue()
+last_audio_state = None
+hifictl = None
+
+
+@api.on_event('startup')
+async def startup_hifictl():
+    global hifictl
+    surround = HarmanKardon_Surround_Receiver("/dev/ttyUSB1")
+    await surround.connect()
+
+    async def collect_states(audio_states):
+        global last_audio_state
+        while True:
+            state = await audio_states.get()
+            if state is None:  # shutdown
+                logger.info("Bye!")
+                break
+            logger.info(state)
+            last_audio_state = state
+            audio_states.task_done()
+
+    hifictl = asyncio.gather(
+        surround.send(audio_commands),
+        surround.recv(audio_states),
+        collect_states(audio_states),
+    )
+
+
+@api.on_event('shutdown')
+async def shutdown_hifictl():
+    await audio_commands.put(None)
+    await hifictl
+
 
 async def communicate(cmd=None):
     if cmd is not None:
-        return {"status": "Executing " + cmd}
-    return {"status": "foobar"}
+        try:
+            category, rest = cmd.split(" ", 1)
+            if category != "audio":
+                raise ValueError(f"Unknown category {category!r}")
+        except Exception as e:
+            logger.warning("Command failed: ", e)
+        else:
+            await audio_commands.put(rest)
+
+    return last_audio_state.json()
 
 
 @api.route("/ws", websocket=True)
